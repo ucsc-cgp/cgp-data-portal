c257ffb273cb45033be9153cbd9f399d591a0d8a
noopdog <dave@clevercanary.com>
2018-10-31 16:48:28 +1000

    deloy test

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -32,7 +32,7 @@ class Header extends React.Component {
                         <Link to="/"><img src={commonsLogo}/></Link>
                     </div>
                     <div className={compStyles.navLinks}>
-                        <li><Link to="/" activeClassName={compStyles.active}>Home</Link></li>
+                        <li><Link to="/" activeClassName={compStyles.active}>No Place Like Home</Link></li>
                         <li><a href={process.env.GATSBY_EXPLORE_URL}>Data Browser</a></li>
                         <li>{this.state.auth ? <a className={compStyles.auth} onClick={this.toggleAuthState}><span>email-me@clevercanary.com</span><span>Logout</span></a> : <a onClick={this.toggleAuthState}>Login</a>}</li>
                     </div>
