7b61a7e520cef7f22e2c060b7007bfe85ea66ef2
noopdog <dave@clevercanary.com>
2018-11-08 10:48:56 -0500

    update explore url

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = "seed" ]; then
-    GATSBY_EXPLORE_URL=https://dev.commons.ucsc-cgp-dev.org/
+    GATSBY_EXPLORE_URL=https://dev.commons.ucsc-cgp-dev.org/explore
 elif [ "$TRAVIS_BRANCH" = "develop" ]; then
-    GATSBY_EXPLORE_URL=https://dev.commons.ucsc-cgp-dev.org/
+    GATSBY_EXPLORE_URL=https://dev.commons.ucsc-cgp-dev.org/explore
 elif [ "$TRAVIS_BRANCH" = "integration" ]; then
-    GATSBY_EXPLORE_URL=https://dev.commons.ucsc-cgp-dev.org/
+    GATSBY_EXPLORE_URL=https://dev.commons.ucsc-cgp-dev.org/explore
 elif [ "$TRAVIS_BRANCH" = "staging" ]; then
-    GATSBY_EXPLORE_URL=https://dev.commons.ucsc-cgp-dev.org/
+    GATSBY_EXPLORE_URL=https://dev.commons.ucsc-cgp-dev.org/explore
 else
-    GATSBY_EXPLORE_URL=https://dev.commons.ucsc-cgp-dev.org/
+    GATSBY_EXPLORE_URL=https://dev.commons.ucsc-cgp-dev.org/explore
 fi
 
 export GATSBY_EXPLORE_URL
